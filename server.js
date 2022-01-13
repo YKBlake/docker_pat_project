226a3624548d7ab0df537cd27e532c68c3a24622
YKBlake <69684201+YKBlake@users.noreply.github.com>
2022-01-13 18:09:56 +0100

    folder

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+var port = 5000;
+
+var server = http.createServer(function (req, res) {   //create web server
+    if (req.url == '/') { //handle requests
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        res.write('<html><body><h1>Hello PAT students !</h1></body></html>');
+        res.end();
+    }
+    else
+        res.end('Invalid Request!');
+});
+
+server.listen(port);
+
+console.log('Node.js web server at port',port,'is running..')
+console.log('Click \'ctrl + c\' to close process')
